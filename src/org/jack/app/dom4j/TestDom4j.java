494516eeaa9561907172fcf37a5e5f803e35c501
jack.liu <jack.liu@digicompass.com>
2014-07-03 17:08:54 +0800

    first commit

---
+++ src/org/jack/app/dom4j/TestDom4j.java
@@ -0,0 +1,9 @@
+package org.jack.app.dom4j;
+import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
+
+public class TestDom4j {
+	Document document = DocumentHelper.createDocument();
+
+}
